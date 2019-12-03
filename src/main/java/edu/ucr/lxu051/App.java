199f5f8333ffd4ff722a26aa36d6907f5e28e7a7
LuxunXu <lxu051@ucr.edu>
2019-12-03 14:32:40 -0800

    init commit

---
+++ src/main/java/edu/ucr/lxu051/App.java
@@ -0,0 +1,13 @@
+package edu.ucr.lxu051;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "\uD83C\uDC05\uD83C\uDC07" );
+    }
+}
---
+++ src/main/java/edu/ucr/lxu051/Util/Tile.java
@@ -0,0 +1,10 @@
+package edu.ucr.lxu051.Util;
+
+enum Simple {
+    DOTS, BAMBOO, CHARACTERS;
+}
+
+public class Tile {
+    private Simple simple;
+    private int number;
+}
