2b90c5af5e9bc032d12fae0857fb563989ba6721
LuxunXu <lxu051@ucr.edu>
2019-12-04 16:54:45 -0800

    updated game

--- src/main/java/edu/ucr/lxu051/App.java
+++ src/main/java/edu/ucr/lxu051/App.java
@@ -16,17 +16,19 @@ public class App
     public static void main( String[] args ) throws IOException
     {
 
-        Hand hand1 = new Hand("NORTH");
-        hand1.initHand("4B 4B 2B 2B 3B 3B 5B 5B 7B 7B 9B 9B 9B");
-        System.out.println(hand1);
-        System.out.println(hand1.isReady());
-
-        Hand hand2 = new Hand("WEST");
-        hand2.initHand("5W 5W 5W 6W 7W 7W 7W 8B 9B 7B 3T 4T 2T");
-        System.out.println(hand2);
-        System.out.println(hand2.isReady());
+//        Hand hand2 = new Hand("WEST");
+//        hand2.initHand("5W 5W 5W 6W 7W 7W 7W 8B 9B 7B 3T 4T 2T");
+//        System.out.println(hand2);
+//        System.out.println(hand2.isReady());
 
 //        HandUtil handUtil = new HandUtil("1133");
 //        System.out.println(handUtil.reduce());
+
+        Game game = new Game();
+        game.initGame();
+        System.out.println(game.getPlayerHand(0));
+        System.out.println(game.getPlayerHand(1));
+        System.out.println(game.getPlayerHand(2));
+        System.out.println(game.getPlayerHand(3));
     }
 }
---
+++ src/main/java/edu/ucr/lxu051/Game.java
@@ -0,0 +1,63 @@
+package edu.ucr.lxu051;
+
+import edu.ucr.lxu051.Util.Hand;
+import edu.ucr.lxu051.Util.Simple;
+import edu.ucr.lxu051.Util.Tile;
+
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.Random;
+
+public class Game {
+    private LinkedList<Tile> tileMountain;
+    private LinkedList<Tile>[] discardedPile;
+    private int tileLeft;
+    private Hand[] players;
+
+    public Game() {
+        tileMountain = new LinkedList<>();
+        discardedPile = new LinkedList[4];
+        players = new Hand[4];
+    }
+
+    public void initGame() {
+        genMountain();
+        Random rnd = new Random();
+        Collections.shuffle(tileMountain, rnd);
+        players[0] = new Hand("EAST"); discardedPile[0] = new LinkedList<>();
+        players[1] = new Hand("SOUTH"); discardedPile[1] = new LinkedList<>();
+        players[2] = new Hand("WEST"); discardedPile[2] = new LinkedList<>();
+        players[3] = new Hand("NORTH"); discardedPile[3] = new LinkedList<>();
+        distributeStartingTiles();
+    }
+
+    public void distributeStartingTiles() {
+        for (int i = 0; i < players.length; i++) {
+            LinkedList<Tile> tempHand = new LinkedList<>();
+            for (int j = 0; j < 13; j++) {
+                tempHand.add(tileMountain.pollFirst());
+                tileLeft--;
+            }
+            players[i].initHand(tempHand);
+        }
+    }
+
+    private void genMountain() {
+        for (Simple simple : Simple.values()) {
+            for (int num = 1; num <= 9; num++) {
+                for (int i = 0; i < 4; i++) {
+                    tileMountain.add(new Tile(simple, num));
+                }
+            }
+        }
+        tileLeft = tileMountain.size();
+    }
+
+    public String getPlayerHand(int pos) {
+        return players[pos].toString();
+    }
+
+    public int getTileLeft() {
+        return tileLeft;
+    }
+}
